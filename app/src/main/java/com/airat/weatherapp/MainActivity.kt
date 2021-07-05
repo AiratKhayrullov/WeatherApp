dfa9b6809fb235592750d990b257238add59f342
Airat <Airatkhayrullov@mail.ru>
2021-07-05 17:29:29 +0400

    Changes v1.1 - Added checking for permissions for gps/network - Added dialog for settings

--- app/src/main/java/com/airat/weatherapp/MainActivity.kt
+++ app/src/main/java/com/airat/weatherapp/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.airat.weatherapp
 
+import android.Manifest
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.location.LocationManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +28,51 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
             startActivity(intent)
         } else {
-            Toast.makeText(this, "Your location provider is already turned ON.", Toast.LENGTH_SHORT).show()
+            Dexter.withActivity(this).withPermissions(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report!!.areAllPermissionsGranted()) {
+                        requestLocationData()
+                    }
+                    if (report.isAnyPermissionPermanentlyDenied) {
+                        Toast.makeText(this@MainActivity, "You've denied location permission", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(permissions: MutableList<PermissionRequest>?, token: PermissionToken?) {
+                    showRationalDialogForPermissions()
+                }
+
+            }).onSameThread().check()
         }
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+                .setMessage("It looks like you've turned off permissions required for this feature. It can be enabled under Application Settings")
+
+                .setPositiveButton("GO TO SETTINGS"){ _, _ ->
+                    try{
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        val uri = Uri.fromParts("package", packageName, null)
+                        intent.data = uri
+                        startActivity(intent)
+                    }catch (e: ActivityNotFoundException){
+                        e.printStackTrace()
+                    }
+                }
+                .setNegativeButton("Cancel"){ dialog, _ ->
+                    dialog.dismiss()
+
+                }
+
+
+    }
+
+    private fun requestLocationData() {
+
     }
 
     private fun isLocationEnabled(): Boolean {
